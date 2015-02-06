e84ed3f430caf320f8d013454d94b3e25a4a69db
vid01 <iuliia.maks@gmail.com>
2015-02-06 10:58:27 -0500

    Branching test!

--- Project/Game/Assets/Resources/Scripts/Mixins/IsTouchable.cs
+++ Project/Game/Assets/Resources/Scripts/Mixins/IsTouchable.cs
@@ -21,20 +21,31 @@ public class IsTouchable : Mixin {
 
 	void IsTouchableUpdate(){}
 
-	public void OnCollisionEnter(Collision other)
+	public void OnCollisionEnter2D(Collision2D other)
 	{
 		foreach(string s in TouchTypes)
 		{
 			if (other.gameObject.GetComponent(s))
 			{
 				// pass reference to this, to consumable if this is consumable
+            // ie. consume item immediately after pickup 
 				IsConsumable cons = GetComponent<IsConsumable>();
 				if (cons)
 					cons.SetRecipient(other.gameObject);
 
+            // stote in Player's inventory
 				IsInventoriable inv = GetComponent<IsInventoriable>();
 				if (inv)
 					inv.SetRecipient(other.gameObject);
+
+            // store in item collection ie. gold in coin purse
+            IsCollectible col = GetComponent<IsCollectible>();
+            if (col)
+               col.SetRecipient(other.gameObject);
+
+            IsPassive pas = GetComponent<IsPassive>();
+            if (pas)
+               pas.SetRecipient(other.gameObject);
 
 				// sendmessage if we actually find a component who can listen for this
 				SendMessage (OnTouchCB);
